a627493627c523eec640e30a75aecf95177d8405
Zeyu He <zhe@mdsol.com>
2014-06-18 19:09:12 -0400

    1. added less and angular bootstrap, customized ionic css to utilize the angular-bootstrap modal  2. tried a different implmentation of the grid using lists

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 
 
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ui.bootstrap'])
 .run(['$rootScope','$http',
   function($rootScope,$http) {
     $http.get('data/studies.json').success(function (data) {
@@ -22,8 +22,13 @@ angular.module('starter.controllers', [])
   }
 ])
 
-.controller('ScenarioCtrl', ['$scope','$rootScope', '$stateParams','$ionicModal',
-  function($scope, $rootScope, $stateParams, $ionicModal) {
+.controller('ScenarioCtrl', ['$scope','$rootScope', '$stateParams',
+  //'$ionicModal',
+  '$modal',
+  function($scope, $rootScope, $stateParams, 
+    //$ionicModal,
+    $modal
+    ) {
     var studyIndex = $stateParams.studyIndex;
     var scenarioIndex = $stateParams.scenarioIndex;
 
@@ -33,20 +38,39 @@ angular.module('starter.controllers', [])
 
     attachGrid($scope);
 
-    $ionicModal.fromTemplateUrl('templates/schedule_modal.html', {
-      scope: $scope,
-      animation: 'slide-in-up'
-    }).then(function (modal) {
-      $scope.modal = modal;
-    });
+    // Use Ionic modal
+    // $ionicModal.fromTemplateUrl('templates/schedule_modal.html', {
+    //   scope: $scope,
+    //   animation: 'slide-in-up'
+    // }).then(function (modal) {
+    //   $scope.modal = modal;
+    // });
 
-    $scope.openModal = function() {
-      $scope.modal.show();
+    // $scope.openModal = function() {
+    //   $scope.modal.show();
+    // }
+
+
+    $scope.openModal = function(activityIndex, visitIndex, size) {
+
+      var modalInstance = $modal.open({
+        templateUrl: 'templates/schedule_modal.html',
+        controller: scheduleModalCtrl,
+        size: size,
+        resolve: {
+          visit: function() {
+            return $scope.data.visits[visitIndex];
+          },
+          activity: function () {
+            return $scope.data.activities[activityIndex];
+          }
+        }
+      })
+
     }
 
     $scope.$on('scheduleGridRendered', function() {
 
-      console.log("!!");
       myScroll = new IScroll('#content', {
         bounce: false,
         scrollX: true,
@@ -57,9 +81,12 @@ angular.module('starter.controllers', [])
 
         // $('#header>table').css('left', myScroll.x );
         // $('#leftHeader>table').css('top', myScroll.y );
+
+        $('#headerList').css('left', myScroll.x );
+        $('#leftHeaderList').css('top', myScroll.y );
         
-        angular.element(document.querySelector('#header>table')).css({"left": myScroll.x });
-        angular.element(document.querySelector('#leftHeader>table')).css({"top": myScroll.y});
+        // angular.element(document.querySelector('#header>table')).css({"left": myScroll.x });
+        // angular.element(document.querySelector('#leftHeader>table')).css({"top": myScroll.y});
       })
     })
 
@@ -83,57 +110,68 @@ angular.module('starter.controllers', [])
   }
 ]);
 
+var scheduleModalCtrl = function ($scope, $modalInstance, activity, visit) {
 
-function adjustGridHeight() {
-    var height = $(window).height();
-    $('#content').css('height', parseInt(height*0.8));
-    $('#leftHeader').css('height', parseInt(height*0.75));
+  $scope.activity = activity;
+  $scope.visit = visit;
+
+  $scope.close = function () {
+    $modalInstance.dismiss('close');
+  }
 }
+
+
+// function adjustGridHeight() {
+//     var height = $(window).height();
+//     $('#content').css('height', parseInt(height*0.8));
+//     $('#leftHeader').css('height', parseInt(height*0.75));
+// }
 
 
 var NUM_OF_VISITS = 50;
 var NUM_OF_ACTIVITIES = 50;
 
-function Cell(txt, quantity) {
-    this.txt = txt;
-    this.on = false;
-    this.quantity = quantity;
-    this.checked = function() {
-        return !this.txt && this.on;
-    };
+
+function Schedule(checked, quantity) {
+    this.checked = checked || false;
+    this.quantity = quantity || 0;
 }
 
-function Grid(header, leftHeader, body) {
-    this.header = header || [];
-    this.leftHeader = leftHeader || [];
-    this.body = body || [];
+function Data(visits, activities, scheduleGrid) {
+    this.visits = visits || [];
+    this.activities = activities || [];
+    this.scheduleGrid = scheduleGrid || [];
+}
+
+function Activity(name) {
+  this.name = name || '';
+}
+
+function Visit(name) {
+  this.name = name || '';
 }
 
 function attachGrid($scope) {
-    var grid = new Grid();
+    var data = new Data();
     for (var i = 0; i < NUM_OF_VISITS; i++) {
-        grid.header.push(new Cell('Visit ' + (i + 1), 0));
+        data.visits.push(new Visit('Visit ' + (i + 1)));
     }
     var row;
     for (var j = 0; j < NUM_OF_ACTIVITIES; j++) {
         row = [];
-        grid.leftHeader.push(new Cell('Activity ' + (j + 1), 0));
+        data.activities.push(new Activity('Activity ' + (j + 1)));
         for (var k = 0; k < NUM_OF_VISITS; k++) {
           if  (j == 5 && k == 5) {
-            var cell = new Cell('',0);
-            cell.on = true;
-            row.push(cell);
+            row.push(new Schedule(true));
           } else {
-            row.push(new Cell('', 0));
+            row.push(new Schedule());
           }
-
-            
         }
-        grid.body.push(row);
+        data.scheduleGrid.push(row);
     }
     $scope.numOfVisits = NUM_OF_VISITS;
     $scope.numOfActivities = NUM_OF_ACTIVITIES;
-    $scope.grid = grid;
+    $scope.data = data;
 }
 
 
